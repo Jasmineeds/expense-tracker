1ecb1a727246708bfa6f7a7bc42f90dbfd20d333
jasmine <jasmine12122005@yahoo.com.tw>
2023-05-02 18:44:16 +0800

    feat: POST create new record function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,9 +1,18 @@
 const express = require("express")
 const router = express.Router()
+const Record = require('../../models/record')
 
 // create new record data
 router.get('/new', (req, res) => {
   res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const record = req.body
+  console.log(record)
+  Record.create(record)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 // edit restaurant data
