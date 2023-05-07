93682cdf994d5831950cd00ed69ae4fc3b6b5a6e
jasmine <jasmine12122005@yahoo.com.tw>
2023-05-07 20:43:35 +0800

    refract: app.js

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+  console.log('dot env required')
+}
+
+// node modules
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
-
-const routes = require('./routes')
 const usePassport = require('./config/passport')
+
+// my modules
+const routes = require('./routes')
 require('./config/mongoose')
 
+// app
 const app = express()
 const PORT = process.env.PORT || 3000
 
+// app engine
 app.engine('hbs', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'hbs')
+
+// app use middleware
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 usePassport(app)
 // use flash to show messages
 app.use(flash())
@@ -30,8 +43,8 @@ app.use((req, res, next) => {
   res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+
+// routes
 app.use(routes)
 
 // start and listen on the Express server
